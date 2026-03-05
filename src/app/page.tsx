eb772520502921fab72d5442b6df5b09b4508b1d
AyloRyd <ayloryd@proton.me>
2026-03-05 21:20:00 +0100

    cleanup data access pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default async function Page() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gray-900 p-8 text-center text-2xl text-gray-100">
-      <h1>T-Drive</h1>
-    </div>
-  );
+  redirect("/f/1");
 }
